4c80812481a74eee9a2263b65d95abf0101c76ef
lihao3 <lihao3@wison.com>
2023-07-12 14:31:13 +0800

    1. 优化注解名称 2. 优化注解判断 3. 添加excludeFromGracefulResponsePackage属性

--- src/main/java/com/feiniaojin/gracefulresponse/advice/NotVoidResponseBodyAdvice.java
+++ src/main/java/com/feiniaojin/gracefulresponse/advice/NotVoidResponseBodyAdvice.java
@@ -58,11 +58,11 @@ public class NotVoidResponseBodyAdvice implements ResponseBodyAdvice<Object> {
             // 判断请求的路径是否在过滤中体现
             if (CollectionUtils.isEmpty(properties.getExcludeFromGracefulResponsePackage())) {
                 // 判断头上是否携带注解, 如果不带
-                return Boolean.FALSE.equals(methodParameter.getMethod().isAnnotationPresent(ExcludeFromGracefulResponse.class));
+                return Boolean.FALSE.equals(method.isAnnotationPresent(ExcludeFromGracefulResponse.class));
             } else {
                 if (properties.getExcludeFromGracefulResponsePackage().stream().anyMatch(item -> ANT_PATH_MATCHER.match(item, className))) {
                     // 判断头上是否携带注解, 如果不带
-                    return Boolean.FALSE.equals(methodParameter.getMethod().isAnnotationPresent(ExcludeFromGracefulResponse.class));
+                    return Boolean.FALSE.equals(method.isAnnotationPresent(ExcludeFromGracefulResponse.class));
                 }
             }
         }
