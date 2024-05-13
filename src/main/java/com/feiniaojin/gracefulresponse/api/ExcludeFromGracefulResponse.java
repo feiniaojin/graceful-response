71258356179b9cd972dcdb8168430a0162e1f8e8
m2c\meng2c <wei.wang@hualongdata.com>
2024-05-13 17:47:06 +0800

    @ExcludeFromGracefulResponse注解修改 1. 可用于controller类上 2. 可用于返回的类型上

--- src/main/java/com/feiniaojin/gracefulresponse/api/ExcludeFromGracefulResponse.java
+++ src/main/java/com/feiniaojin/gracefulresponse/api/ExcludeFromGracefulResponse.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  *
  * @author lihao3
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface ExcludeFromGracefulResponse {
